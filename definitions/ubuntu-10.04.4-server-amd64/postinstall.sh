bb9868b35025a9b25dd9cc1f96f45f27e75452e4
Erik Bakker <erik.bakker@lunatech.com>
2012-08-29 09:54:25 +0200

    Upgrade ruby to 1.9.3-p125, ruby gems to 1.8.24

--- definitions/ubuntu-10.04.4-server-amd64/postinstall.sh
+++ definitions/ubuntu-10.04.4-server-amd64/postinstall.sh
@@ -22,22 +22,22 @@ apt-get -y install nfs-common
 # can install their own Rubies using packages or however.
 # We must install the 1.8.x series since Puppet doesn't support
 # Ruby 1.9 yet.
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
-tar xvzf ruby-1.9.3-p125.tar.gz
-cd ruby-1.9.3-p125
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
+tar xvzf ruby-1.9.3-p194.tar.gz
+cd ruby-1.9.3-p194
 ./configure --prefix=/opt/ruby
 make
 make install
 cd ..
-rm -rf ruby-1.9.3-p125*
+rm -rf ruby-1.9.3-p194*
 
-# Install RubyGems 1.8.17
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.17.tgz
-tar xzf rubygems-1.8.17.tgz
-cd rubygems-1.8.17
+# Install RubyGems 1.8.24
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
+tar xzf rubygems-1.8.24.tgz
+cd rubygems-1.8.24
 /opt/ruby/bin/ruby setup.rb
 cd ..
-rm -rf rubygems-1.8.17*
+rm -rf rubygems-1.8.24*
 
 # Installing chef
 /opt/ruby/bin/gem install chef --no-ri --no-rdoc
